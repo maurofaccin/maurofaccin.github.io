4696cdb29de234b2f94fb37b2a1778f1c7303129
Mauro Faccin <mauro.fccn@gmail.com>
2020-01-06 14:23:32 +0100

    tb cluster 2019

--- _slides_to_images.sh
+++ _slides_to_images.sh
@@ -1,11 +1,23 @@
+#!/usr/bin/eval bash
 
-for f in slides.*; do
-	pdftocairo -png -scale-to-x 1200 -scale-to-y -1 slides.pdf slide
-done
-for f in poster.*; do
-	pdftocairo -png -scale-to-x 1200 -scale-to-y -1 poster.pdf poster
-done
+if [ -e slides.pdf ]; then
+    pdftocairo -png -scale-to-x 1200 -scale-to-y -1 slides.pdf slide
+elif [ -e poster.pdf ]; then
+    pdftocairo -png -scale-to-x 1200 -scale-to-y -1 poster.pdf poster
+fi
 
-trimage -d .
+# for f in slides.*; do
+#     pdftocairo -png -scale-to-x 1200 -scale-to-y -1 slides.pdf slide
+# done
+# for f in poster.*; do
+#     pdftocairo -png -scale-to-x 1200 -scale-to-y -1 poster.pdf poster
+# done
+
+for f in *.png; do
+    echo $f
+    pngquant --output temp.png $f
+    mv temp.png $f
+done
+# trimage -d .
 
 ls
---
+++ content/slides/2019-11-14-tb-cluster-leuven/index.md
@@ -0,0 +1,16 @@
+---
+date: 2019-11-14
+title: "Data-driven active-case finding"
+subtitle: TB cluster 2019 @ Leuven (Belgium)
+contrib: talk
+---
+
+# TB Cluster meeting
+
+### REGA Institute (Leuven), 14 Nov. 2019
+
+* Axel Silroy on _"The challenges of Mtb research in the age of cryo-EM"_
+* Mauro Faccin on _"Data-driven active-case finding"_
+* Theresa Pattery on _"Improving patient management and treatment adherence to TB with a differentiated service delivery model using Connect for Life(tm) mobile health (mhealth) platform"_
+* Lies Laenen on _"Implementing automated whole genome sequencing pipelines in the clinical environment"_
+* Pieter Leyssen/Winston Chiu: _"Next-generation phenotypic drug susceptibility testing for mycobacteria"_
