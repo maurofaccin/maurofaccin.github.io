840af7acdbf14c948ee3ad42c45caecb8c70c643
Mauro Faccin <mauro.fccn@gmail.com>
2021-04-28 12:19:52 +0200

    add small script to prepare a background with alpha gradient

---
+++ src/background_addalpha.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DIR=static/images
+ME=$(dirname "$0")
+
+for source in "${ME}/bg-"*;
+do
+    echo "$source"
+    fname=$(basename "${source}")
+    convert "${source}"\
+        -colorspace sRGB\
+        -alpha set -background none -channel A\
+        -sparse-color barycentric '0,0 white 0,%[h] none'\
+        +channel\
+        "${DIR}/${fname%.jpg}.png"
+done
+
+
