1f83f2ec8230aeed9a0546292f5ea2e048478fea
Mauro Faccin <mauro.fccn@gmail.com>
2022-02-24 17:16:04 +0100

    use yaml safe_load

--- src/cv/render_jinja.py
+++ src/cv/render_jinja.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+"""Render the template."""
 from sys import argv
 
+import yaml
 from jinja2 import Environment
-from yaml import load
 
 yam = argv[1]
 tmp = argv[2]
 with open(yam) as fd:
-    data = load(fd.read())
+    data = yaml.safe_load(fd.read())
 
 with open(tmp) as fd:
     template = fd.read()
