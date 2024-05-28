d2344062c9daccb49afd4ae73583818e3eafe628
Mauro Faccin <mauro.fccn@gmail.com>
2024-05-28 12:28:50 +0200

    [feat] add filter to interpret it bd and tt from markdown to tex or html

--- src/cv/render_jinja.py
+++ src/cv/render_jinja.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 """Render the template."""
+
+import re
+from functools import partial
 from sys import argv
 
 import yaml
 from jinja2 import Environment
+
+MD = {
+    "tex": {"it": r"\\emph{\1}", "bd": r"\\textbf{\1}", "tt": r"\\texttt{\1}"},
+    "html": {"it": r"<i>\1</i>", "bd": r"<b>\1</b>", "tt": r"<var>\1</var>"},
+}
+
+
+def md2tex(text: str, kind: str = "tex") -> str:
+    """Convert simple markdown to latex."""
+    if not isinstance(text, str):
+        return text
+    # code
+    newtext = re.sub("`([^`]*)`", MD[kind]["tt"], text)
+    # bold
+    newtext = re.sub("\*\*([^\*]*)\*\*", MD[kind]["bd"], newtext)
+    newtext = re.sub("__([^\*]*)__", MD[kind]["bd"], newtext)
+    # italic
+    newtext = re.sub("\*([^\*]*)\*", MD[kind]["it"], newtext)
+    newtext = re.sub("_([^\*]*)_", MD[kind]["it"], newtext)
+    return newtext
+
 
 yam = argv[1]
 tmp = argv[2]
@@ -14,6 +38,7 @@ with open(tmp) as fd:
     template = fd.read()
 
 env = Environment(lstrip_blocks=True, trim_blocks=True)
+
 if tmp.endswith("tex.jinja"):
     env.block_start_string = "[[%"
     env.block_end_string = "%]]"
@@ -21,5 +46,9 @@ if tmp.endswith("tex.jinja"):
     env.variable_end_string = "]]"
     env.comment_start_string = "[[="
     env.comment_end_string = "=]]"
+    env.filters["md"] = partial(md2tex, kind="tex")
+elif tmp.endswith("html.jinja"):
+    env.filters["md"] = partial(md2tex, kind="html")
+
 render = env.from_string(template)
 print(render.render(data))
